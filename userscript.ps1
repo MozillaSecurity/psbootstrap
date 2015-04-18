69cac6c01ce171a194fc1159e0babd030676c4ea
Gary Kwong <gary@rumblingedge.com>
2015-04-17 19:41:10 -0700

    Revert 98d3fb3, this time commenting that user-data on EC2 runs as SYSTEM user which is different from the Administrator user.

--- userscript.ps1
+++ userscript.ps1
@@ -10,9 +10,11 @@ Param()
 Set-StrictMode -Version Latest  # Helps to catch errors
 
 # Directories
-$DOWNLOADS = "$HOME\Downloads"
-$SSH_DIR = "$HOME\.ssh"
-$TREES = "$HOME\trees"
+# $MY_HOME is needed because user-data starts up as SYSTEM user which is *not* Administrator.
+$MY_HOME = "C:\Users\Administrator"
+$DOWNLOADS = "$MY_HOME\Downloads"
+$SSH_DIR = "$MY_HOME\.ssh"
+$TREES = "$MY_HOME\trees"
 $MC_REPO = "$TREES\mozilla-central"
 
 # Versions
@@ -125,16 +127,16 @@ Write-Verbose "Setting up configurations..."
 Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\Windows Error Reporting' -Name DontShowUI -Value 1 | out-null
 # Create a shortcut to ~ in Favorites. Adapted from http://stackoverflow.com/a/9701907
 $WshShell = New-Object -comObject WScript.Shell
-$Shortcut = $WshShell.CreateShortcut("$HOME\Links\Administrator.lnk")
-$Shortcut.TargetPath = "$HOME"
+$Shortcut = $WshShell.CreateShortcut("$MY_HOME\Links\Administrator.lnk")
+$Shortcut.TargetPath = "$MY_HOME"
 $Shortcut.Save()
 # Create a shortcut to C:\mozilla-build in Favorites. Adapted from http://stackoverflow.com/a/9701907
 $WshShell2 = New-Object -comObject WScript.Shell
-$Shortcut2 = $WshShell2.CreateShortcut("$HOME\Links\mozilla-build.lnk")
+$Shortcut2 = $WshShell2.CreateShortcut("$MY_HOME\Links\mozilla-build.lnk")
 $Shortcut2.TargetPath = "C:\mozilla-build"
 $Shortcut2.Save()
 # Mercurial settings
-New-Item "$HOME\.hgrc" -type file -value "[ui]
+New-Item "$MY_HOME\.hgrc" -type file -value "[ui]
 merge = internal:merge
 ssh = $MOZILLABUILD_INSTALLDIR\msys\bin\ssh.exe -C -v
 
@@ -160,7 +162,7 @@ $Utf8NoBomEncoding = New-Object System.Text.UTF8Encoding($False)
                                 (Get-Content $MOZILLABUILD_START_SCRIPT_FULL_PATH), $Utf8NoBomEncoding)
 
 Write-Verbose "Cloning fuzzing repository..."
-Measure-Command { & $HG_BINARY --cwd $HOME clone -e "$MOZILLABUILD_INSTALLDIR\msys\bin\ssh.exe -i $SSH_DIR\id_dsa -o stricthostkeychecking=no -C -v" `
+Measure-Command { & $HG_BINARY --cwd $MY_HOME clone -e "$MOZILLABUILD_INSTALLDIR\msys\bin\ssh.exe -i $SSH_DIR\id_dsa -o stricthostkeychecking=no -C -v" `
     <repository location> fuzzing |
     Out-Host }
 Write-Verbose "Finished cloning fuzzing repository."
