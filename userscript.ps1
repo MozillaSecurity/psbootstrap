a2ab4c3c9006d732b0ab6978d7b6a0aec634d711
Gary Kwong <gary@rumblingedge.com>
2015-04-22 18:33:33 -0700

    Disable the Application Compatibility Engine and the Program Compatibility Assistant.

--- userscript.ps1
+++ userscript.ps1
@@ -145,6 +145,13 @@ Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\Windows Error Reporting
 New-Item -Path 'HKLM:\Software\Microsoft\Windows\Windows Error Reporting' -Name LocalDumps | out-null
 Set-ItemProperty -Path 'HKLM:\Software\Microsoft\Windows\Windows Error Reporting\LocalDumps' -Name DumpCount -Value 500 | out-null
 Set-ItemProperty -Path 'HKLM:\Software\Microsoft\Windows\Windows Error Reporting\LocalDumps' -Name DumpType -Value 1 | out-null
+# Get Group Policy Settings Reference from http://www.microsoft.com/en-us/download/details.aspx?id=25250
+# Disable Application Compatibility Engine and Program Compatibility Assistant
+New-Item -Path 'HKLM:\Software\Policies\Microsoft\Windows' -Name AppCompat | out-null
+Set-ItemProperty -Path 'HKLM:\Software\Policies\Microsoft\Windows\AppCompat' -Name DisableEngine -Value 1 | out-null
+Set-ItemProperty -Path 'HKLM:\Software\Policies\Microsoft\Windows\AppCompat' -Name DisablePCA -Value 1 | out-null
+& gpupdate /force | out-null
+
 # Create a shortcut to ~ in Favorites. Adapted from http://stackoverflow.com/a/9701907
 $WshShell = New-Object -comObject WScript.Shell
 $Shortcut = $WshShell.CreateShortcut("$MY_HOME\Links\Administrator.lnk")
