7dd3b8ada10ccf31678429762937b78a3b45116e
Gary Kwong <gary@rumblingedge.com>
2015-04-22 18:36:19 -0700

    Reorder registry key-setting steps.

--- userscript.ps1
+++ userscript.ps1
@@ -67,6 +67,27 @@ Function ExtractArchive ($fileName, $dirName) {
     Write-Verbose "Finished extracting $fileName ."
 }
 
+Write-Verbose "Setting up configurations..."
+# Windows Registry settings
+# Disable the Windows Error Dialog
+Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\Windows Error Reporting' -Name DontShowUI -Value 1 | out-null
+# Turn on crash dumps
+New-Item -Path 'HKLM:\Software\Microsoft\Windows\Windows Error Reporting' -Name LocalDumps | out-null
+Set-ItemProperty -Path 'HKLM:\Software\Microsoft\Windows\Windows Error Reporting\LocalDumps' -Name DumpCount -Value 500 | out-null
+Set-ItemProperty -Path 'HKLM:\Software\Microsoft\Windows\Windows Error Reporting\LocalDumps' -Name DumpType -Value 1 | out-null
+# Get Group Policy Settings Reference from http://www.microsoft.com/en-us/download/details.aspx?id=25250
+# Disable Application Compatibility Engine and Program Compatibility Assistant
+New-Item -Path 'HKLM:\Software\Policies\Microsoft\Windows' -Name AppCompat | out-null
+Set-ItemProperty -Path 'HKLM:\Software\Policies\Microsoft\Windows\AppCompat' -Name DisableEngine -Value 1 | out-null
+Set-ItemProperty -Path 'HKLM:\Software\Policies\Microsoft\Windows\AppCompat' -Name DisablePCA -Value 1 | out-null
+& gpupdate /force | out-null
+
+# Create a shortcut to ~ in Favorites. Adapted from http://stackoverflow.com/a/9701907
+$WshShell = New-Object -comObject WScript.Shell
+$Shortcut = $WshShell.CreateShortcut("$MY_HOME\Links\Administrator.lnk")
+$Shortcut.TargetPath = "$MY_HOME"
+$Shortcut.Save()
+
 # Microsoft Visual Studio 2013 Community Edition
 $VS2013COMMUNITY_FTP = "http://go.microsoft.com/?linkid=9863608"
 $VS2013COMMUNITY_SETUP = "$DOWNLOADS\vs_community.exe"
@@ -136,27 +157,6 @@ StrictHostKeyChecking no
 Host hg.mozilla.org
 User fuzzbots' | out-null
 Write-Verbose "Finished setting up ssh configurations."
-
-Write-Verbose "Setting up configurations..."
-# Windows Registry settings
-# Disable the Windows Error Dialog
-Set-ItemProperty -Path 'HKCU:\Software\Microsoft\Windows\Windows Error Reporting' -Name DontShowUI -Value 1 | out-null
-# Turn on crash dumps
-New-Item -Path 'HKLM:\Software\Microsoft\Windows\Windows Error Reporting' -Name LocalDumps | out-null
-Set-ItemProperty -Path 'HKLM:\Software\Microsoft\Windows\Windows Error Reporting\LocalDumps' -Name DumpCount -Value 500 | out-null
-Set-ItemProperty -Path 'HKLM:\Software\Microsoft\Windows\Windows Error Reporting\LocalDumps' -Name DumpType -Value 1 | out-null
-# Get Group Policy Settings Reference from http://www.microsoft.com/en-us/download/details.aspx?id=25250
-# Disable Application Compatibility Engine and Program Compatibility Assistant
-New-Item -Path 'HKLM:\Software\Policies\Microsoft\Windows' -Name AppCompat | out-null
-Set-ItemProperty -Path 'HKLM:\Software\Policies\Microsoft\Windows\AppCompat' -Name DisableEngine -Value 1 | out-null
-Set-ItemProperty -Path 'HKLM:\Software\Policies\Microsoft\Windows\AppCompat' -Name DisablePCA -Value 1 | out-null
-& gpupdate /force | out-null
-
-# Create a shortcut to ~ in Favorites. Adapted from http://stackoverflow.com/a/9701907
-$WshShell = New-Object -comObject WScript.Shell
-$Shortcut = $WshShell.CreateShortcut("$MY_HOME\Links\Administrator.lnk")
-$Shortcut.TargetPath = "$MY_HOME"
-$Shortcut.Save()
 # Create a shortcut to C:\mozilla-build in Favorites. Adapted from http://stackoverflow.com/a/9701907
 $WshShell2 = New-Object -comObject WScript.Shell
 $Shortcut2 = $WshShell2.CreateShortcut("$MY_HOME\Links\mozilla-build.lnk")
