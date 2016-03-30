92207019286861071aad89fd541e27d5eb823167
Gary Kwong <gary@rumblingedge.com>
2016-03-30 14:08:20 +0900

    Now that we have Mercurial 3.7.x and above in MozillaBuild, stop using a mozilla-central bundle and clone directly instead.

--- userscript.ps1
+++ userscript.ps1
@@ -174,10 +174,6 @@ Write-Verbose "Installing $GIT_FILE_WITH_DIR ..."
 & $GIT_FILE_WITH_DIR /SILENT | out-null
 Write-Verbose "Finished installing $GIT_FILE_WITH_DIR ."
 
-# mozilla-central bundle
-$MCBUNDLE_FTP = "http://ftp.mozilla.org/pub/mozilla.org/firefox/bundles/mozilla-central.hg"
-$MCBUNDLE_FILE = "$DOWNLOADS\mozilla-central.hg"
-DownloadBinary $MCBUNDLE_FTP $MCBUNDLE_FILE
 
 Write-Verbose "Setting up ssh configurations..."
 New-Item $SSH_DIR -type directory | out-null
@@ -227,19 +223,7 @@ Write-Verbose "Finished cloning FuzzManager repository."
 
 Write-Verbose "Unbundling mozilla-central..."
 New-Item $TREES -type directory | out-null
-& $HG_BINARY --cwd $TREES init mozilla-central | out-null
-New-Item "$MC_REPO\.hg\hgrc" -type file -value '[paths]
-
-default = https://hg.mozilla.org/mozilla-central
-' | out-null
-& $HG_BINARY -R $MC_REPO unbundle "$DOWNLOADS\mozilla-central.hg" | Write-Output
-Write-Verbose "Finished unbundling mozilla-central."
-
-Write-Verbose "Updating mozilla-central..."
-& $HG_BINARY -R $MC_REPO update -C default | Write-Output
-& $HG_BINARY -R $MC_REPO pull | Write-Output
-& $HG_BINARY -R $MC_REPO update -C default | Write-Output
-Write-Verbose "Finished updating mozilla-central."
+& $HG_BINARY --cwd $TREES clone https://hg.mozilla.org/mozilla-central $MC_REPO | out-null
 
 Write-Verbose "Commencing fuzzing."
 # Only for certain machines: & schtasks.exe /create /ru Administrators /sc onlogon /delay 0000:01 /tr $MOZILLABUILD_START_SCRIPT_FULL_PATH /tn jsFuzzing
